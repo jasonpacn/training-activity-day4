3179de168be56b6c0664e5c746278affaef50ab7
Jason Patigayon <jason.patigayon@accenture.com>
2025-08-11 04:09:42 +0000

    2nd commit

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
 ], 
 /**
  * @param {typeof sap.ui.core.mvc.Controller} Controller
  * @param {typeof sap.m.MessageToast} MessageToast
  */
-function (Controller, MessageToast) {
+function (Controller, MessageToast, MessageBox) {
     "use strict";
 
     return Controller.extend("com.training.exer1patigayon.controller.MainView", {
@@ -22,26 +23,46 @@ function (Controller, MessageToast) {
         fnDisplayMsg: function (sMsg){
             MessageToast.show(sMsg);
         },
+
         onChangeMOP: function (oEvent) {
             var sSelectedKey = oEvent.getParameter("selectedItem").getProperty("key");
             var oMobileLabel = this.getView().byId("idLblPhone");
             var oMobileInput = this.getView().byId("idInputPhone");
-
-            if (sSelectedKey === "GCASH"){
+            var oCCLabel = this.getView().byId("idLblCC");
+            var oCCInput = this.getView().byId("idInputCC");
+            //show the mobile num field
+            if (sSelectedKey === "GCASH") {
                 oMobileLabel.setVisible(true);
                 oMobileInput.setVisible(true);
-            } else {
+                oCCLabel.setVisible(false);
+                oCCInput.setVisible(false);
+            }
+            //show credit card input for CC
+            else if (sSelectedKey === "CC") {
                 oMobileLabel.setVisible(false);
                 oMobileInput.setVisible(false);
+                oCCLabel.setVisible(true);
+                oCCInput.setVisible(true);
+            }
+            //else(COD)
+            else {
+                oMobileLabel.setVisible(false);
+                oMobileInput.setVisible(false);
+                oCCLabel.setVisible(false);
+                oCCInput.setVisible(false);
             }
         },
         onPressCheckout: function (){
-            var oInputFNameValue = this.getView().byId("idInptFName").getValue();
+            var oView = this.getView();
+            var sFName = oView.byId("idInptFName").getValue().trim();
+            var sLName = oView.byId("idInptLName").getValue().trim();
 
-            // Check if first name is blank
-            if (oInputFNameValue === ""){
-                sap.m.MessageToast.show("Required Field is blank"); 
+            if (sFName === "" && sLName === "") {
+                MessageBox.error("Both First Name and Last Name are required.");
+                return;
             }
+
+            MessageToast.show("Checkout successful!");
         },
 
 
